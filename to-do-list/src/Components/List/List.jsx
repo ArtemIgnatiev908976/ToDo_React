bf7be280d7d4d1a9f3aa1dc552cfcd500ac9c061
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-28 15:06:22 +0400

    Добавил стили для кнопки удаления задачи

--- to-do-list/src/Components/List/List.jsx
+++ to-do-list/src/Components/List/List.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import './List.scss'
 import classNames from 'classnames';
 import Badge from '../Badge/Badge'
+import  removeSvg from '../../assets/img/remove.svg'
+
 
 const List = ({items, isRemovable, onClick}) =>{
     // const obj={a:1, b: 1}; //деструкторизация -взять свойста и превратить в переменные
@@ -16,6 +18,8 @@ const List = ({items, isRemovable, onClick}) =>{
                         {/*//передаем строчку которая содержит текст с помощью библиотеки classesnames*/}
                         <i>{item.icon ? (item.icon) : (<Badge color={item.color} />)}</i>
                     <span>{item.name}</span>
+                        {isRemovable && <img className="list__remove-icon" src={removeSvg} alt="Remove icon"/> }
+
                     </li>
                 )
                 )
