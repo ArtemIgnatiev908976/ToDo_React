a8b3a0815c6794a013a17de3ce31f1d3a110b9e8
ArtemIgnatiev908976 <ignatev.artem.nik@gmail.com>
2020-04-29 19:52:55 +0400

    bugs

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,8 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import listSvg from './assets/img/list.svg';
 import addSvg from './assets/img/add.svg';
+<<<<<<< HEAD
 import axios from  'axios'
 import {List, AddList, Tasks} from "./Components";
+=======
+import List from './Components/List/List'
+import AddList from "./Components/AddList/AddList";
+import Tasks from "./Components/Tasks/Tasks";
+>>>>>>> parent of e2340d3... Перенес подключение компонентов в один  файл
 import DB from './assets/db.json'
 
 function App() {
--- to-do-list/src/Components/index.js
+++
@@ -1,5 +0,0 @@
-
-export {default as Badge} from './Badge/Badge';
-export {default as List} from './List/List';
-export {default as Tasks} from './Tasks/Tasks';
-export {default as AddList} from './AddList/AddList';
